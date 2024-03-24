4f00298e2d31f38d8641cd1ded9550735933fe48
Janelle Cunningham <janellecunningham@live.com>
2024-03-24 17:24:00 +0000

    accounts

---
+++ accounts.py
@@ -0,0 +1,15 @@
+# Hides account numbers for privacy
+# Author: Janelle Cunningham
+
+# $ python accounts.py
+
+# Please enter a 10 digit account number: 1234567890
+
+
+
+account_number = '1234567890'
+print('X' * 6 + account_number [-4:])
+
+
+
+
